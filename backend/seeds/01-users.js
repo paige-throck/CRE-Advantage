f3743025db3512b3049f1e217c7a5a979401b76f
Paige Throckmorton <path4942@gmail.com>
2018-03-12 13:04:03 -0500

    Addedd seeds

--- backend/seeds/01-users.js
+++ backend/seeds/01-users.js
@@ -1,13 +1,26 @@
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+ // Deletes ALL existing entries
+ return knex('properties').del()
+   .then(function () {
+     // Inserts seed entries
+     return knex('properties').insert([
+       {
+         address: '119 Nueces St, Austin, TX 78701',
+         prop_owner: 'Amanda',
+         num_suites: 10,
+         lat: 30.265557030,
+         lang: -97.749699097,
+         prospective_prop: false
+       },
+       {
+         address: '901 Little Texas Lane, Austin, TX 78745',
+         prop_owner: 'Melissa',
+         num_suites: 10,
+         lat: 30.1976950,
+         lang: -97.768543097,
+         prospective_prop: false
+       }
+     ]);
+   });
 };
