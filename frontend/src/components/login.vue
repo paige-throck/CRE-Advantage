57fc69db3a8615db22bc5b3d518079de3db68e5c
Paige Throckmorton <path4942@gmail.com>
2018-03-13 11:33:07 -0500

    Pushing new frontend

---
+++ frontend/src/components/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">
+    <h1>Login Page</h1>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data () {
+      return {
+        msg: 'Login Page'
+      }
+    }
+  }
+</script>
