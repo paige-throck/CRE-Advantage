5177e295491c0d65ecfe14ba932af2bc03bd4eb5
Paige Throckmorton <path4942@gmail.com>
2018-03-16 15:34:33 -0500

    Working on tasks routes and components

--- client/src/components/Tasks.vue
+++ client/src/components/Tasks.vue
@@ -3,10 +3,20 @@
 <div class="tasks">
 
 <router-link to="/profile">CRE Advantage</router-link>
+
+
 <Nav></Nav>
+
+{{ tasksArr }}
+
+<ul id="taskList">
+  <li v-for="task in tasksArr[0]">
+    {{ task.item }}
+    {{ task.task_date }}
+  </li>
+</ul>
+
 </div>
-
-
 </template>
 
 <script>
@@ -14,13 +24,29 @@
 import axios from 'axios';
 import Nav from './Nav'
 
+console.log(localStorage.id, "User id");
+
 export default {
+
   name: 'Tasks',
+  data(){
+    return {
+       tasksArr: []
+    }
+  },
+  mounted: function(){
+    let self = this;
+    let id = window.localStorage.id;
+    axios.get(`http://localhost:8881/tasks/${id}`)
+    .then(function(results){
+      console.log(results.data[0], 'results from get tassks');
+     self.tasksArr.push(results.data);
 
-
+    })
+  }
 }
 </script>
-
+local
 <style>
 
 </style>
