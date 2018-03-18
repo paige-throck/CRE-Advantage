d9cf1f0248cede054454d5499c11d68ad5f5fe65
Paige Throckmorton <path4942@gmail.com>
2018-03-18 16:17:32 -0500

    Working on task list

--- client/src/components/Tasks.vue
+++ client/src/components/Tasks.vue
@@ -1,71 +1,42 @@
 
 <template>
 <div class="row tasks">
-<Nav></Nav>
+  <Nav></Nav>
 
-<div class = "row tasksContent">
-{{ tasksArr }}
+<div class="form">
+    <div class="input-group">
+      <input type="text" class="form-control" ng-model="task">
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="button" v-on:click="addTask()"><span
+          class="glyphicon glyphicon-plus"></span> Add Task</button>
+      </span>
+    </div><!-- /input-group -->
+  </div>
+<hr/>
 
-<ul id="taskList">
-  <li v-for="task in tasksArr[0]">
-    {{ task.item }}
-    {{ task.task_date }}
-  </li>
-</ul>
-
-
-<div class="container">
-    <div class="row">
-        <div class="col-md-6">
-            <div class="todolist not-done">
-             <h1>Todos</h1>
-                <input type="text" class="form-control add-todo" placeholder="Add todo">
-                    <button id="checkAll" class="btn btn-success">Mark all as done</button>
-
-                    <hr>
-                    <ul id="sortable" class="list-unstyled">
-                    <li class="ui-state-default">
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" value="" />Take out the trash</label>
-                        </div>
-                    </li>
-                    <li class="ui-state-default">
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" value="" />Buy bread</label>
-                        </div>
-                    </li>
-                    <li class="ui-state-default">
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" value="" />Teach penguins to fly</label>
-                        </div>
-                    </li>
-                </ul>
-                <div class="todo-footer">
-                    <strong><span class="count-todos"></span></strong> Items Left
-                </div>
-            </div>
+    <ul class="list-group" v-show="tasksArr[0].length > 0">
+      <li class="list-group-item clearfix task" v-for="task in tasksArr[0]" v-bind:class="{disabled: task.done}">
+        <p class="lead">{{task.item}}</p>
+        <div>
+          <span class="pull-right">
+        <button class="btn btn-default btn-xs"><span class="glyphicon glyphicon-pencil"
+          v-on:click="editTask($index)"></span></button>
+          <button class="btn btn-primary btn-xs" v-show="!task.done"><span class="glyphicon glyphicon-ok"
+          v-on:click="doneTask($index)"></span></button>
+          <button class="btn btn-primary btn-xs" v-show="task.done"><span class="glyphicon glyphicon-repeat"
+          v-on:click="unDoneTask($index)"></span></button>
+          <button class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"
+          v-on:click="deleteTask($index)"></span></button>
+          </span>
         </div>
-        <div class="col-md-6">
-            <div class="todolist">
-             <h1>Already Done</h1>
-                <ul id="done-items" class="list-unstyled">
-                    <li>Some item <button class="remove-item btn btn-default btn-xs pull-right"><span class="glyphicon glyphicon-remove"></span></button></li>
+      </li>
+    </ul>
+  </div>
 
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
-
-</div>
 </div>
 </template>
 
 <script>
-
 import axios from 'axios';
 import Nav from './Nav'
 
@@ -74,25 +45,27 @@ console.log(localStorage.id, "User id");
 export default {
 
   name: 'Tasks',
-  data(){
+  data() {
     return {
-       tasksArr: []
+      tasksArr: []
     }
   },
-  mounted: function(){
+  mounted: function() {
     let self = this;
     let id = window.localStorage.id;
     axios.get(`http://localhost:8881/tasks/${id}`)
-    .then(function(results){
-      console.log(results.data[0], 'results from get tassks');
-     self.tasksArr.push(results.data);
+      .then(function(results) {
+        console.log(results.data[0], 'results from get tassks');
+        self.tasksArr.push(results.data);
 
-    })
+      })
   }
 }
 </script>
 
 
 
-<style>
+<style scoped>
+
+
 </style>
