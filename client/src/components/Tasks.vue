edbcedb7f0a9c57eb69ea103b48458fdd9c7e932
Paige Throckmorton <path4942@gmail.com>
2018-03-21 14:53:06 -0500

    Why you no work edit task?

--- client/src/components/Tasks.vue
+++ client/src/components/Tasks.vue
@@ -1,115 +1,115 @@
 <template>
 <div class="row tasks">
-  <Nav></Nav>
+<Nav></Nav>
 
-  <span class="pull-right">
-  <button class="btn btn-danger" v-on:click="clearList($event)"><span
+<span class="pull-right">
+<button class="btn btn-danger" v-on:click="clearList($event)"><span
 ></span>Clear Tasks</button>
-  </span>
+</span>
 
-  <br></br>
+<br></br>
 
 
-  <!--  New Task Form-->
-  <form class="form-tasks" @submit.prevent="addTask">
-    <div class="row">
+<!--  New Task Form-->
+<form class="form-tasks" @submit.prevent="addTask">
+  <div class="row">
 
-      <div class="col-sm-2">
-        <span class="input-group-btn">
-      <button class="btn btn-default" type="submit"><span
-    class="glyphicon glyphicon-plus"></span> Add Task</button>
+    <div class="col-sm-2">
+      <span class="input-group-btn">
+    <button class="btn btn-default" type="submit"><span
+  class="glyphicon glyphicon-plus"></span> Add Task</button>
+      </span>
+    </div>
+
+    <div class="col-sm-8">
+      <input type="text" class="form-control" v-model="newTask.item" placeholder="Add a task" required autofocus>
+    </div>
+
+    <div class="col-sm-2">
+      <div class='input-group date' ref="datetimepicker">
+        <input type="text" class="form-control" v-model="newTask.task_date" />
+        <span class="input-group-addon">
+        <span class="glyphicon glyphicon-calendar"></span>
+        </span>
+      </div>
+    </div>
+  </div>
+</form>
+
+<!-- Tasks List -->
+
+<div class="row taskList">
+  <ul class="list-group" v-for="task in tasksArr[0]" v-model="tasksArr">
+
+
+    <li class="list-group-items clearfix task" v-bind:class="{complete:task.completed, notComplete: !task.completed}">
+
+      <div class="list-items">
+        <span class="pull-left">
+
+        <div class="lead">  {{task.item}} </div>
+    </span>
+        <span class="pull-left">
+        <div class="lead">  &nbsp; {{task.task_date}}</div>
+    </span>
+      </div>
+      <div>
+        <span class="pull-right">
+
+<button class="btn btn-default btn-xs" type="button" v-on:click="showEditForm = !showEditForm"><span class="glyphicon glyphicon-pencil"></span></button>
+
+        <button class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-ok"
+  v-on:click="completeTask($event, task.id)"></span></button>
+
+
+
+        <button class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"
+v-on:click="deleteTask($event, task.id)"></span></button>
         </span>
       </div>
 
-      <div class="col-sm-8">
-        <input type="text" class="form-control" v-model="newTask.item" placeholder="Add a task" required autofocus>
-      </div>
-
-      <div class="col-sm-2">
-        <div class='input-group date' ref="datetimepicker">
-          <input type="text" class="form-control" v-model="newTask.task_date" />
-          <span class="input-group-addon">
-          <span class="glyphicon glyphicon-calendar"></span>
-          </span>
-        </div>
-      </div>
-    </div>
-  </form>
-
-  <!-- Tasks List -->
-
-  <div class="row taskList">
-    <ul class="list-group" v-for="task in tasksArr[0]" v-model="tasksArr">
-
-
-      <li class="list-group-items clearfix task" v-bind:class="{complete:task.completed, notComplete: !task.completed}">
-
-        <div class = "list-items">
-          <!-- <span class="pull-left">
-          <button class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-ok"
-    v-on:click="completeTask($event, task.id)"></span></button>
-  </span> -->
-
-        <span class="pull-left">
-
-          <div class="lead">  {{task.item}} </div>
-      </span>
-        <span class="pull-left">
-          <div class="lead">  &nbsp; {{task.task_date}}</div>
-      </span>
-    </div>
-      <div>
-          <span class="pull-right">
-<button class="btn btn-default btn-xs" data-toggle="collapse" href="#edit-task" role="button" aria-expanded="false" aria-controls="suites"><span class="glyphicon glyphicon-pencil"
-  v-on:click="updateTask($event, task.id, task.item)"></span></button>
-
-          <button class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-ok"
-    v-on:click="completeTask($event, task.id)"></span></button>
-
-
-
-          <button class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"
-  v-on:click="deleteTask($event, task.id)"></span></button>
-          </span>
-        </div>
-
-  <!-- Edit Task Form inside List -->
-        <form class="collapse multi-collapse" id="edit-task">
+      <!-- Edit Task Form inside List -->
+      <div class = "col">
+        <div v-if="showEditForm">
+      <form  id="edit-task" @submit.prevent="editTask">
         <br></br>
-          <div class = "col-sm-2">
+        <div class="col-sm-2">
+        </div>
+
+        <div class="col-sm-8">
+
+          <div class="row">
+            <span class="input-group-btn">
+              <button class="btn btn-default" type="submit"><span
+            class="glyphicon glyphicon-plus"></span> Update Task</button>
+            </span>
+
+          </div>
+          <div class="row">
+            <input type="text" class="form-control" id="edited-email" :placeholder="task.item" v-model="editedTask.item">
+            {{task.item}}
+            {{editedTask.item}}
           </div>
 
-        <div class = "col-sm-8">
-
-        <div class = "row">
-                  <span class="input-group-btn">
-                <button class="btn btn-default" type="submit"><span
-              class="glyphicon glyphicon-plus"></span> Update Task</button>
-                  </span>
-
+          <div class="row">
+            <div class='input-group date' ref="datetimepicker">
+              <input type="text" class="form-control" v-model="newTask.task_date" />
+              <span class="input-group-addon">
+                  <span class="glyphicon glyphicon-calendar"></span>
+              </span>
+            </div>
+          </div>
         </div>
-        <div class ="row">
-                  <input type="text" class="form-control"  :placeholder="task.item"  required autofocus>
+        <div class="col-sm-2">
         </div>
-
-        <div class = "row">
-                  <div class='input-group date' ref="datetimepicker">
-                    <input type="text" class="form-control" v-model="newTask.task_date" />
-                    <span class="input-group-addon">
-                    <span class="glyphicon glyphicon-calendar"></span>
-                    </span>
-                  </div>
-        </div>
-
-        </div>
-        <div class = "col-sm-2">
-        </div>
-        </form>
-      </li>
-    </ul>
+      </form>
+    </div>
   </div>
+    </li>
+  </ul>
+</div>
 
-
+{{tasksArr[0]}}
 </div>
 </template>
 
@@ -119,159 +119,158 @@ import Nav from './Nav'
 
 
 export default {
-  name: 'Tasks',
-  data() {
-    return {
-      tasksArr: [],
-      newTask: {
-        user_id: window.localStorage.id,
-        item: '',
-        task_date: ''
-      },
-      completed: false
-      // t:true,
-      // f:false
-    }
+name: 'Tasks',
+data() {
+  return {
+    tasksArr: [],
+    newTask: {
+      user_id: window.localStorage.id,
+      item: '',
+      task_date: ''
+    },
+    editedTask:{
+      item: ""
+    },
+    showEditForm: false
+
+  }
+},
+mounted: function() {
+  this.getTasks()
+  // $(this.$refs.datetimepicker).datetimepicker()
+},
+methods: {
+  // updateTime(event) {
+  //   this.$emit('input', event.target.value);
+  // },
+
+  getTasks() {
+    let self = this;
+    let id = window.localStorage.id;
+
+    axios.get(`http://localhost:8881/tasks/${id}`)
+      .then(function(results) {
+        console.log(results.data, 'Results from Get Tasks');
+        self.tasksArr = [];
+        self.tasksArr.push(results.data);
+      })
   },
-  mounted: function() {
-    this.getTasks()
-    // $(this.$refs.datetimepicker).datetimepicker()
+  addTask() {
+    console.log(this.newTask, "New Task being created!!");
+    let self = this;
+    let id = window.localStorage.id;
+    axios.post(`http://localhost:8881/tasks/${id}`, this.newTask)
+      .then(function() {
+        self.newTask.item = ""
+        self.getTasks();
+      }).catch(function(error) {
+        console.log(error);
+      });
   },
-  methods: {
-    // updateTime(event) {
-    //   this.$emit('input', event.target.value);
-    // },
 
-    getTasks() {
-      let self = this;
-      let id = window.localStorage.id;
+  editTask() {
 
-      axios.get(`http://localhost:8881/tasks/${id}`)
-        .then(function(results) {
-          console.log(results.data, 'Results from Get Tasks');
-          self.tasksArr = [];
-          self.tasksArr.push(results.data);
-        })
-    },
-    addTask() {
-      console.log(this.newTask, "New Task being created!!");
-      let self = this;
-      let id = window.localStorage.id;
-      axios.post(`http://localhost:8881/tasks/${id}`, this.newTask)
-        .then(function() {
-          self.newTask.item = ""
-          self.getTasks();
-        }).catch(function(error) {
-          console.log(error);
-        });
-    },
+    let self = this;
+    let id = window.localStorage.id;
 
-    updateTask(event, editTaskId, editItem){
-      console.log(editTaskId, 'task id', editItem, 'item');
-    },
+    self.showEditForm = !self.showEditForm;
 
-    editTask(event, taskId) {
-      let self = this;
-      let id = window.localStorage.id;
+    axios.put(`http://localhost:8881/tasks/${id}/${taskId}`, this.task)
+      .then(function() {
+        // self.getTasks();
+      }).catch(function(error) {
+        console.log(error);
+      });
+  },
 
-      axios.put(`http://localhost:8881/tasks/${id}/${taskId}`, this.task)
-        .then(function() {
-          self.getTasks();
-        }).catch(function(error) {
-          console.log(error);
-        });
-    },
+  completeTask(event, taskId) {
+    console.log(taskId, 'task id in complete task');
+    let self = this;
+    let id = window.localStorage.id;
 
-    completeTask(event, taskId) {
-      console.log(taskId, 'task id in complete task');
-      let self = this;
-      let id = window.localStorage.id;
+    axios.put(`http://localhost:8881/tasks/${id}/${taskId}/completeTask`)
+      .then(function() {
+        self.getTasks();
+      }).catch(function(error) {
+        console.log(error);
+      });
+  },
 
-      axios.put(`http://localhost:8881/tasks/${id}/${taskId}/completeTask`)
-        .then(function() {
-          self.getTasks();
-        }).catch(function(error) {
-          console.log(error);
-        });
-    },
+  deleteTask(event, taskId) {
+    let self = this;
+    let id = window.localStorage.id;
 
-    deleteTask(event, taskId) {
-      let self = this;
-      let id = window.localStorage.id;
+    console.log(taskId, "the task id");
+    console.log(id, 'user id');
 
-      console.log(taskId, "the task id");
-      console.log(id, 'user id');
+    axios.delete(`http://localhost:8881/tasks/${id}/${taskId}`)
+      .then(function() {
+        self.getTasks();
+      }).catch(function(error) {
+        console.log(error);
+      });
+  },
 
-      axios.delete(`http://localhost:8881/tasks/${id}/${taskId}`)
-        .then(function() {
-          self.getTasks();
-        }).catch(function(error) {
-          console.log(error);
-        });
-    },
-
-    clearList: function(event) {
-      let self = this;
-      axios.delete(`http://localhost:8881/tasks/clearCompletedTasks`)
+  clearList: function(event) {
+    let self = this;
+    axios.delete(`http://localhost:8881/tasks/clearCompletedTasks`)
       .then(function() {
         self.getTasks();
       }).catch(function(error) {
         console.log(error);
       });
 
-    }
-
-
   }
+
+
+}
 
 }
 </script>
 
 <style scoped>
 .tasks {
-  margin: 5%;
+margin: 5%;
 }
 
 .form-tasks {
-  margin-bottom: 2%;
+margin-bottom: 2%;
 }
 
 ul {
-  list-style: none;
+list-style: none;
 }
 
 .complete {
-  background-color: gainsboro;
+background-color: gainsboro;
 }
 
 .notComplete {
-  background-color: white;
+background-color: white;
 }
 
 .list-group-true {
-  list-style: none;
-  border-radius: 5px;
+list-style: none;
+border-radius: 5px;
 }
 
-#edit-task{
-  position: fixed;
-  top:50%;
-  left:50%;
-  margin-top:-37.5%;
-  margin-left:-37.5%;
-  width:75%;
-  height:75%;
-  background-color: grey;
-  z-index:10;
+#edit-task {
+position: fixed;
+top: 50%;
+left: 50%;
+margin-top: -37.5%;
+margin-left: -37.5%;
+width: 75%;
+height: 75%;
+background-color: grey;
+z-index: 10;
 }
 
-.edit-task-form{
-  position: fixed;
-  top:25%;
-  left:25%;
+.edit-task-form {
+position: fixed;
+top: 25%;
+left: 25%;
 
-  background-color: blue;
+background-color: blue;
 }
-
-
 </style>
