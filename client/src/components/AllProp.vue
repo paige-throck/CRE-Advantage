adf761d79f9f0bebad17112dcf875abb5c588743
Paige Throckmorton <path4942@gmail.com>
2018-03-22 09:57:23 -0500

    Pushing all the things

---
+++ client/src/components/AllProp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row allProp">
+
+
+
+
+
+</div>
+</div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+
+export default {
+  name: 'AllProp',
+
+  }
+
+
+</script>
+
+<style scoped>
+.home{
+  margin:5%;
+}
+</style>
